9f67b8f2f67c84b8673fb8b35a9378cb18a77247
Roshan112113 <rajeevguptajalalpur@gmail.com>
2026-03-30 10:14:06 +0530

    Add files via upload

---
+++ C3/20_03_2026/ReverseAry.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ReverseAry{
+
+    static void reverseArray(int arr[], int n) {
+        for(int i = n-1; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Reversed Array:");
+
+        reverseArray(arr, n);
+
+        sc.close();
+    }
+}
